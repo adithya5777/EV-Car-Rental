1f715789473a3804abfd2816d66d3780b46c53a0
SaathvikMV <117590588+SaathvikMV@users.noreply.github.com>
2022-12-29 20:58:15 +0530

    Add files via upload

---
+++ ev/bill.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Sonko Car Rental</title>
+	<meta charset="utf-8">
+	<meta name="author" content="pixelhint.com">
+	<meta name="description" content="La casa free real state fully responsive html5/css3 home page website template" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+
+<body>
+
+	<section class="wrapper">
+		<?php
+		include 'header.php';
+		?>
+
+		<section class="caption">
+			<h1 style="text-align: center; font-size:xxx-large;"><span style="font-weight: 100;">Find the Best </span>CarForYou</h1>
+		</section>
+        
+    
+           
+    
+    </section> <!--  end listing section  -->
+
+
+
+<?php
+include 'footer.php';
+?>
+
+</body>
+
+</html>
